b242bca012cad0918cea6682528d1e0b19095a3a
sixseven7ib <jeissonorozcolozano14@gmail.com>
2026-04-28 10:30:39 -0500

    Update and rename README.md to Variables.py

---
+++ Variables.py
@@ -0,0 +1,16 @@
+#Inicializando variables y colocandoles un valor
+nombre= "Jeisson Andrés Orozco Lozano" (aquí obviamente cambian mi nombre por su nombre completo)
+edad= 14 (ponen su edad aquí)
+Estado= True
+
+#Esta linea me imprime el contenido de una variable
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
+
+#Uso de la funcion input()
+nombre=input("¿Cual es tu nombre? ")
+edad=input("¿Cual es tu edad? ")
+
+#Mostrar el resultado capturado con input()
+print("Bienvenido", nombre)
+print("Tu edad es", edad)
